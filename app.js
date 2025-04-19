ec11f3e787cd0e378f2e1381c735b9e88ee61d82
joshyemane <joshyemane1@gmail.com>
2025-04-19 15:22:57 -0700

    service card edits

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
-const toggleButton = document.getElementsByClassName("navbar__toggle");
-[0];
+const toggleButton = document.getElementsByClassName("navbar__toggle")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
 toggleButton.addEventListener("click", () => {
